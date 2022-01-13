f2191e227ebc8b97badbc7ca7c37f0510607a1d6
Andrea Del Corto <andrea.delcorto@vedrai.com>
2022-01-13 03:08:26 +0100

    add style import

--- src/client/demo-bootstrap/index.js
+++ src/client/demo-bootstrap/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SumEditor from './components/SumEditor';
 
+import './styles.css';
+
 ReactDOM.render(<SumEditor />, document.getElementById('index'));
